35af540c0ac305def2496bb23e732c05fc58a7c1
gabelluardo <gabriele.belluardo@outlook.it>
2019-01-13 15:57:25 +0100

    aggiunto flag di shell

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Poliappelli
 
-Script per riunire tutte le date degli appelli
+Script per ricordare tutte le date degli appelli del PoliTo
 
 ### Utili
 
-Fare riferimento a :
+Fare riferimento a:
 
 -   [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) per parsificare le table
 -   [Selenium](https://www.seleniumhq.org/) per navigazione web
 -   [Getpass](https://alektos.blogspot.com/2011/06/inserire-password-con-python.html) per nascondere le password
 -   [BeautifulTable](https://beautifultable.readthedocs.io/en/latest/index.html) per l'output della tabella
+-   [Argparser](https://docs.python.org/3.6/library/argparse.html#module-argparse) per i flag da shell
 
 ### TODO
 
@@ -17,6 +18,8 @@ Fare riferimento a :
 -   [ ] import automatico su notable
 -   [x] scriverlo meglio
 -   [x] ordine di data/alfabetico
+    -   [ ] ordine inverso
 -   [ ] navigazione web: inserimento di solo user e password per accedere al portale polito
 -   [x] visualizzazione da terminale
 -   [ ] memorizzazione utente. (salvare password???)
+-   [x] flag da shell
--- script.py
+++ script.py
@@ -1,14 +1,15 @@
 #!./venv/bin/python3
 
 import sys
+import argparse
+import urllib.request
+import os.path
 from bs4 import BeautifulSoup
 from beautifultable import BeautifulTable
 from datetime import datetime as dt
-import urllib.request
-import os.path
 
 
-class TABparser:
+class Scraper:
     def __init__(self):
         self.esami = list()
 
@@ -18,17 +19,11 @@ class TABparser:
     def _openurl(self, url):
         return urllib.request.urlopen(url).read()
 
-    def _sort(self, key='Data'):
-        # TODO: aggiungere flag per l'ordinamento
-        if key is 'Data':
-            self.esami.sort(key=lambda materia: dt.strptime(
-                materia[2], "%d-%m-%Y %H:%M"))
-
-    def parse(self, path, tag='table'):
-        ## parsifica tutte le table che trova
-        ## nella pagina/file html e ne salva
-        ## i campi nella lista self.esami
-        ## ordinati per data dell'esame
+    def tab_parse(self, path, tag='table'):
+        # parsifica tutte le table che trova
+        # nella pagina/file html e ne salva
+        # i campi nella lista self.esami
+        # ordinati per data dell'esame
 
         target = self._openfile(path) if os.path.isfile(
             path) else self._openurl(path)
@@ -38,38 +33,62 @@ class TABparser:
                 row = [cell.text.strip() for cell in tr.find_all('td')]
                 if row.__len__() > 0:
                     self.esami.append(row)
-
-        self._sort()
-
-    def output(self, filename='esami.md', terminal=False):
-        ## crea una tabella in sintassi markdonw
-        ## della lista self.esami che verra'
-        ## stampata su terminale oppure su file
-
-        table = BeautifulTable(
-            max_width=200, default_alignment=BeautifulTable.ALIGN_LEFT)
-        table.set_style(BeautifulTable.STYLE_MARKDOWN)
-        table.column_headers = ['Nome', 'Data', 'Tipo', 'Scadenza', 'Aula']
-
-        for materia in self.esami:
-            table.append_row([
-                materia[1], materia[2], materia[4], materia[6], ''])
-
-        if terminal:
-            print(table)
-        else:
-            file = open(filename, 'w')
-            file.write("## Esami\n\n")
-            file.write(str(table))
-            file.close()
+        return self.esami
 
 
 def main():
-    # TODO: flag linea di comando
+    # inizializzazione degli argomenti
+    # che posso essere passati da shell
 
-    hp = TABparser()
-    hp.parse('test.html')
-    hp.output(terminal=False)
+    parser = argparse.ArgumentParser(
+        description='Script le date degli appelli del PoliTo.')
+    parser.add_argument('-s', '--sort', dest='sorted', nargs='?', default='Data', const='Data', type=str,
+                        help='ordinamento delle materie (default: Data)', choices=['Nome', 'Data', 'Tipo', 'Scadenza'])
+    parser.add_argument('-o', '--output', nargs='?', dest='output', const='esami.md',
+                        type=str, help="scrive l'output su file (default: esami.md)")
+    args = parser.parse_args()
+
+    # core dello script
+    esami = Scraper().tab_parse('test.html')
+    print_table(esami, args.output, args.sorted)
+
+
+def print_table(esami, filename, sort_flag):
+    # crea una tabella (in sintassi markdown)
+    # della lista esami che verra'
+    # ordinata secondo il sort_flag
+    # stampata su terminale oppure su file
+    sort(esami, sort_flag)
+
+    table = BeautifulTable(
+        max_width=200, default_alignment=BeautifulTable.ALIGN_LEFT)
+    table.set_style(BeautifulTable.STYLE_MARKDOWN)
+    table.column_headers = ['Nome', 'Data', 'Tipo', 'Scadenza', 'Aula']
+
+    for materia in esami:
+        table.append_row([
+            materia[1], materia[2], materia[4], materia[6], ''])
+
+    if filename is None:
+        print(table)
+    else:
+        file = open(filename, 'w')
+        file.write("## Esami\n\n")
+        file.write(str(table))
+        file.close()
+
+
+def sort(target, key='Data'):
+    if key == 'Nome':
+        target.sort(key=lambda materia: materia[1])
+    elif key == 'Tipo':
+        target.sort(key=lambda materia: materia[4])
+    elif key == 'Scadenza':
+        target.sort(key=lambda materia: dt.strptime(
+            materia[6], "%d-%m-%Y %H:%M"))
+    else:
+        target.sort(key=lambda materia: dt.strptime(
+            materia[2], "%d-%m-%Y %H:%M"))
 
 
 if __name__ == '__main__':
