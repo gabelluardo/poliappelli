88f3d7ce0418641d6a5122bae02c20cde640159e
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2021-09-18 11:32:34 +0200

    chore(deps): update

--- deps.ts
+++ deps.ts
@@ -1,12 +1,12 @@
 /* STL */
-export { existsSync } from "https://deno.land/std@0.95.0/fs/mod.ts";
-export { assert } from "https://deno.land/std@0.95.0/testing/asserts.ts";
+export { existsSync } from "https://deno.land/std@0.107.0/fs/mod.ts";
+export { assert } from "https://deno.land/std@0.107.0/testing/asserts.ts";
 
 /* libs */
 export {
   Command,
   CompletionsCommand,
-} from "https://deno.land/x/cliffy@v0.18.2/command/mod.ts";
+} from "https://deno.land/x/cliffy@v0.19.5/command/mod.ts";
 export { default as Kia } from "https://raw.githubusercontent.com/gabelluardo/kia/master/mod.ts";
-export { default as puppeteer } from "https://deno.land/x/puppeteer@9.0.0/mod.ts";
-export { Table } from "https://deno.land/x/cliffy@v0.18.2/table/mod.ts";
+export { default as puppeteer } from "https://deno.land/x/puppeteer@9.0.1/mod.ts";
+export { Table } from "https://deno.land/x/cliffy@v0.19.5/table/mod.ts";
